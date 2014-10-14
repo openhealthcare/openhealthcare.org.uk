d622735022ccf3ed7026f4dcb07a95a26bd2b461
David Miller <david@deadpansincerity.com>
2014-10-14 09:30:30 +0000

    Site updated at 2014-10-14 09:30:29 UTC

---
+++ assets/js/ohc.js
@@ -0,0 +1,23 @@
+function hello(what){
+    event.preventDefault();
+    email = $('#'+what).val()
+    $.post('http://ohcucontakt.deadpansincerity.com/former.php', {
+        z_name: 'Unknown',
+        z_requester: email,
+        z_subject: 'Open Health Care UK website quick form',
+        z_description: 'More information about Open Health Care UK'
+    },function(){ window.location = window.location.href});
+    return false;
+}
+
+function contactform(){
+    event.preventDefault();
+    
+    $.post('http://ohcucontakt.deadpansincerity.com/former.php', {
+        z_name: $('#name').val(),
+        z_requester: $('#email').val(),
+        z_subject: 'OPAL website contact form',
+        z_description: $('#message').val(),
+    }, function(){ window.location = window.location.href});
+    return false;
+}
